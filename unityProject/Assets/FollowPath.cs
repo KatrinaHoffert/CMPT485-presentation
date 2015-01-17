6d2cc023054f58b89c1522fb243129f7051f416c
dylanbrodiefafard <dylan.brodie.fafard@gmail.com>
2015-01-16 18:11:05 -0600

    Created scene to view textures in a 3d Application.

---
+++ unityProject/Assets/FollowPath.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPath : MonoBehaviour
+{
+    public Vector3[] posPath;
+    public float speed;
+    private int step;
+
+	void Start () 
+    {
+        step = 0;
+        transform.position = posPath[step];
+	}
+	
+	void FixedUpdate () 
+    {
+        if (Vector3.Distance(transform.position, posPath[step]) < 0.01f)
+        {
+            if (++step == posPath.Length)
+            {
+                this.enabled = false;
+                Application.Quit();
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, posPath[step], speed * Time.deltaTime);
+        }
+	}
+}
